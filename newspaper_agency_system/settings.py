beec539a5af82de821e87029604001f99c62f520
Marharyta Marchenko <ritanika12@gmail.com>
2024-05-19 12:00:16 +0200

    add crispy-forms and crispy-bootstrap5 to settings

--- newspaper_agency_system/settings.py
+++ newspaper_agency_system/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'news',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +70,10 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 WSGI_APPLICATION = 'newspaper_agency_system.wsgi.application'
 
